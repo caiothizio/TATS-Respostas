fec6f311eed538ec52cd734b57e54f76071fdf12
caiothizio <caiothizio@alunos.utfpr.edu.br>
2018-08-10 11:27:58 -0300

    Add Questao Pratica Atividade 02

---
+++ atividade02/src/atv02/SenhaValidator.java
@@ -0,0 +1,15 @@
+package atv02;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author caio.thizio
+ */
+public interface SenhaValidator {
+    public boolean verificar(String senha);
+}
